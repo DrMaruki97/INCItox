91e64ebe2865159a035023764ba999c6f2495fec
DrMaruki97 <luca.giovagnoli1997@outlook.it>
2024-06-15 13:59:33 +0200

    varianzioni funzioni CIR

--- CosmeticIngredientReview.py
+++ CosmeticIngredientReview.py
@@ -4,15 +4,14 @@ import requests as req
 import string 
 from bs4 import BeautifulSoup
 
-def aggiorna_database_cir():
+def aggiorna_database(database:dict):
 
-    url_base = 'https://cir-reports.cir-safety.org/'
     request = 'https://cir-reports.cir-safety.org/FetchCIRReports/?&pagingcookie=%26lt%3bcookie%20page%3d%26quot%3b1%26quot%3b%26gt%3b%26lt%3bpcpc_name%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_ingredientidname%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_cirrelatedingredientsid%20last%3d%26quot%3b%7bC223037E-F278-416D-A287-2007B9671D0C%7d%26quot%3b%20first%3d%26quot%3b%7b940AF697-52B5-4A3A-90A6-B9DB30EF4A7E%7d%26quot%3b%20%2f%26gt%3b%26lt%3b%2fcookie%26gt%3b&page='
-    database = {}
-
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+    
     for i in range(1,3):
 
-        table = req.get(f'{request}{i}')
+        table = req.get(f'{request}{i}',headers=header)
         table = table.json()
 
         for el in table['results']:
@@ -20,7 +19,7 @@ def aggiorna_database_cir():
             database[(el['pcpc_ciringredientname'],el['pcpc_ingredientname'])] = [el['pcpc_ingredientid']]
 
 
-def pdf_date_cir(chiave : tuple, database : dict):
+def get_source(chiave : tuple, database : dict):
 
     url_base = 'https://cir-reports.cir-safety.org/'    
     web_page = req.get(f'{url_base}cir-ingredient-status-report/?id={database[chiave][0]}')
--- Project_code.py
+++ Project_code.py
@@ -1,1 +1,6 @@
 # file per lo sript del progetto di ricerca della tossicità di una molecola dato il suo INCI
+
+import requests as req
+import CosmeticIngredientReview as cir
+import string 
+from bs4 import BeautifulSoup
