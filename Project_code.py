54c970c85220ecb452e9a32764a1d91cd3393ba0
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-07-10 14:41:32 +0200

    Cleaning repository

--- Project_code.py
+++
@@ -1,9 +0,0 @@
-# file per lo sript del progetto di ricerca della tossicità di una molecola dato il suo INCI
-
-import requests as req
-import CosmeticIngredientReview as cir
-import string 
-from bs4 import BeautifulSoup
-
-database = {}
-cir.aggiorna_database(database)
