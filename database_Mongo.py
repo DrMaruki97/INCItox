68015fe154c28a4224f69c32542f22826830a743
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-07-10 19:45:26 +0200

    MongoDB database creation

---
+++ database_Mongo.py
@@ -0,0 +1,58 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import requests as req
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+
+uri = "mongodb+srv://lucagiovagnoli:t7g^Fyi7zpN!Liw@ufs13.dsmvdrx.mongodb.net/?retryWrites=true&w=majority&appName=UFS13"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+    
+
+db = client['INCI']
+coll = db['Ingredients']
+
+request = 'https://cir-reports.cir-safety.org/FetchCIRReports/?&pagingcookie=%26lt%3bcookie%20page%3d%26quot%3b1%26quot%3b%26gt%3b%26lt%3bpcpc_name%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_ingredientidname%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_cirrelatedingredientsid%20last%3d%26quot%3b%7bC223037E-F278-416D-A287-2007B9671D0C%7d%26quot%3b%20first%3d%26quot%3b%7b940AF697-52B5-4A3A-90A6-B9DB30EF4A7E%7d%26quot%3b%20%2f%26gt%3b%26lt%3b%2fcookie%26gt%3b&page='
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+url_base = 'https://cir-reports.cir-safety.org/' 
+
+
+for i in range(1,3):
+
+    table = req.get(f'{request}{i}',headers=header)
+    table = table.json()
+    for el in tqdm(table['results']):
+            if not db.coll.find_one({"Nome_comune":f"{el['pcpc_ingredientname']}"}):
+
+                link = f'https://cir-reports.cir-safety.org/cir-ingredient-status-report/?id={el['pcpc_ingredientid']}'
+                web_page = req.get(link,headers=header)
+                page = BeautifulSoup(web_page.text,'html.parser')
+                righe = page.find_all('tr')
+                if len(righe)>1:
+                    for i in range(1,len(righe)):
+                        riga = i      
+                        report = righe[i].a['href']
+                        if report[0] == '.':
+                            break
+                    if report[0] == '.':
+                            final_url = url_base + report[report.index('/')+1:]
+                            date = righe[riga].find_all('td')[-1].text
+                    else:
+                        final_url = ''
+                        date = ''
+                else:
+                    final_url = ''
+                    date = ''    
+
+                if final_url:
+                     db.coll.insert_one({"Nome_comune":f"{el['pcpc_ingredientname']}",
+                                         "INCI_name":f"{el['pcpc_ciringredientname']}",
+                                         "main_link":link,
+                                         "pdf_link":final_url,
+                                         "pdf_date":date})
+                        
+                   
+                    
+                
