df39831bd60dd2a330170589734d60eec5f7f365
DrMaruki97 <luca.giovagnoli1997@outlook.it>
2024-07-10 19:40:54 +0200

    Completed database_redis initiation

--- database_redis.py
+++ database_redis.py
@@ -5,13 +5,14 @@ from bs4 import BeautifulSoup
 
 
 
-
 r = redis.Redis(
     host='redis-11492.c300.eu-central-1-1.ec2.redns.redis-cloud.com',
     port = 11492,
     password='0ssFnSEhNJ6Hn7JVtznKkpOuAD1ffdtR',
     decode_responses=True
 )
+
+r.flushdb()
 
 request = 'https://cir-reports.cir-safety.org/FetchCIRReports/?&pagingcookie=%26lt%3bcookie%20page%3d%26quot%3b1%26quot%3b%26gt%3b%26lt%3bpcpc_name%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_ingredientidname%20last%3d%26quot%3bPEG-50%20Stearate%26quot%3b%20first%3d%26quot%3b1%2c10-Decanediol%26quot%3b%20%2f%26gt%3b%26lt%3bpcpc_cirrelatedingredientsid%20last%3d%26quot%3b%7bC223037E-F278-416D-A287-2007B9671D0C%7d%26quot%3b%20first%3d%26quot%3b%7b940AF697-52B5-4A3A-90A6-B9DB30EF4A7E%7d%26quot%3b%20%2f%26gt%3b%26lt%3b%2fcookie%26gt%3b&page='
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
@@ -50,7 +51,7 @@ for i in range(1,3):
                     r.rpush('list:ingredients',f'{el['pcpc_ingredientname']}')
                     if final_url:
                         r.set(f'pdf:{el['pcpc_ingredientname']}',final_url)
-                        r.set(f'data_pdf:{el['pcpc_ingredientname']}',date.text)
+                        r.set(f'data_pdf:{el['pcpc_ingredientname']}',date)
                         
                    
                     
@@ -60,6 +61,6 @@ for i in range(1,3):
                     
                     if final_url:
                         r.set(f'pdf:{el['pcpc_ingredientname']}',final_url)
-                        r.set(f'data_pdf:{el['pcpc_ingredientname']}',date.text)
+                        r.set(f'data_pdf:{el['pcpc_ingredientname']}',date)
                         r.set(f'pdf:{el['pcpc_ciringredientname']}',final_url)
-                        r.set(f'data_pdf:{el['pcpc_ciringredientname']}',date.text)
+                        r.set(f'data_pdf:{el['pcpc_ciringredientname']}',date)
